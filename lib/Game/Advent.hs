6bcade4e7ff242863b8831f6ccb1542170c459c6
James Jackson <j.e.m.jackson@gmail.com>
2022-10-20 04:43:32 -0500

    Initial version

---
+++ lib/Game/Advent.hs
@@ -0,0 +1,28 @@
+module Game.Advent where
+
+import System.TimeIt (timeItT)
+import Text.Printf ( printf )
+import Control.Exception (evaluate)
+
+dataFile :: String -> IO String
+dataFile s = readFile $ "/home/james/Code/advent/2017/data/" ++ s
+
+data Day = Day
+  { dayName :: String,
+    dayPart1 :: IO String,
+    dayPart2 :: IO String
+  }
+
+runDay :: Day -> IO ()
+runDay day = do
+  runPart "1" =<< timeItT (evaluate =<< dayPart1  day)
+  runPart "2" =<< timeItT (evaluate =<< dayPart2  day)
+  where
+    runPart part (elapsed, result) =
+      printf
+        "% 8s Part %s\t\tResult: %- 60s Time: %0.4f S\n"
+        (if part == "1" then "Day " ++ dayName day ++ "," else "")
+        part
+        result
+        elapsed
+
